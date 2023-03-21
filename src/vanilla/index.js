58a59110bd47c6010fc08ad294b7a0c19f49f6e8
Dylan Lu <dylan_lu@gss.com.tw>
2023-03-21 17:24:01 +0800

    refine chat text

--- src/vanilla/index.js
+++ src/vanilla/index.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 const line_name = document.getElementById("line_name").value
                 const real_name = document.getElementById("real_name").value
                 const agree = document.getElementById("agree").checked
-                const message = `${line_name}-${real_name}-${line_id}-${agree}`
+                const message = `/submit_privacy_form\nID: ${line_id}\nLINE: ${line_name}\n姓名: ${real_name}\n同意: ${agree}`
                 liff.sendMessages([{ type: 'text', text: message }])
                 .then(() => {
                     console.log('message sent')
