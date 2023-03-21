f58214dbc250529fcadb31db04eb68b4f5c378a2
Dylan Lu <dylan_lu@gss.com.tw>
2023-03-21 16:29:10 +0800

    add sample privacy agreement

--- src/vanilla/index.js
+++ src/vanilla/index.js
@@ -1,11 +1,33 @@
 import './index.css';
 import liff from '@line/liff'
 
+function getProfile() {
+    // mock data
+    return {"displayName":"John Doe","userId":"U12345678901234567890123456789012","pictureUrl":"https://example.com/abcdefghijklmn","statusMessage":"Hello, LINE!"}
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   liff
     .init({ liffId: process.env.LIFF_ID })
     .then(() => {
         console.log("Success! you can do something with LIFF API here.")
+        if(!liff.isLoggedIn()) {
+            liff.login()
+        } else {
+            console.log("Already logged in.")
+            const profile = getProfile()
+            const real_name = document.getElementById("real_name")
+            const real_name_col = document.getElementById("real_name_col")
+            real_name_col.attributes.removeNamedItem("hidden")
+            const line_id = document.getElementById("line_id")
+            // set value and cancel hidden attribute
+            line_id.setAttribute("value", profile.userId)
+            const line_name = document.getElementById("line_name")
+            line_name.setAttribute("value", profile.displayName)
+            const line_name_col = document.getElementById("line_name_col")
+            line_name_col.attributes.removeNamedItem("hidden")   
+        }
+        
     })
     .catch((error) => {
         console.log(error)
