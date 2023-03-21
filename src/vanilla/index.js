92977ae53d1a0a2dbe51cd07bf9e5b18660f2025
Dylan Lu <dylan_lu@gss.com.tw>
2023-03-21 16:34:37 +0800

    fix async get profile

--- src/vanilla/index.js
+++ src/vanilla/index.js
@@ -1,11 +1,6 @@
 import './index.css';
 import liff from '@line/liff'
 
-function getProfile() {
-    // mock data
-    return liff.getProfile()
-}
-
 document.addEventListener("DOMContentLoaded", function() {
   liff
     .init({ liffId: process.env.LIFF_ID })
@@ -15,17 +10,18 @@ document.addEventListener("DOMContentLoaded", function() {
             liff.login()
         } else {
             console.log("Already logged in.")
-            const profile = getProfile()
-            const real_name = document.getElementById("real_name")
-            const real_name_col = document.getElementById("real_name_col")
-            real_name_col.attributes.removeNamedItem("hidden")
-            const line_id = document.getElementById("line_id")
-            // set value and cancel hidden attribute
-            line_id.setAttribute("value", profile.userId)
-            const line_name = document.getElementById("line_name")
-            line_name.setAttribute("value", profile.displayName)
-            const line_name_col = document.getElementById("line_name_col")
-            line_name_col.attributes.removeNamedItem("hidden")   
+            const profile = liff.getProfile().then(profile => {
+                const real_name = document.getElementById("real_name")
+                const real_name_col = document.getElementById("real_name_col")
+                real_name_col.attributes.removeNamedItem("hidden")
+                const line_id = document.getElementById("line_id")
+                // set value and cancel hidden attribute
+                line_id.setAttribute("value", profile.userId)
+                const line_name = document.getElementById("line_name")
+                line_name.setAttribute("value", profile.displayName)
+                const line_name_col = document.getElementById("line_name_col")
+                line_name_col.attributes.removeNamedItem("hidden")  
+            })
         }
         
     })
